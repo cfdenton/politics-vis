09e8c2ac9f9090663da895fe6991a22f399fc230
Carl Denton <carldenton@college.harvard.edu>
2016-11-12 23:15:51 -0500

    .

--- scrape.py
+++ scrape.py
@@ -4,7 +4,7 @@ import sys
 import re
 import requests
 from bs4 import BeautifulSoup
-from newspaper import Article
+from langdetect import detect
 
 # check that a url is valid
 def valid_url(url):
@@ -22,24 +22,34 @@ def valid_url(url):
 # check that to urls are not from the same domain
 def same_domain(url1, url2, print_out):
     #?(.*?)\.(?:com|au.uk|co\.in)/
-    reg = re.compile(r'\.[a-zA-Z0-9]*\.(?:com|au.uk|co\.in|co\.uk|gov|org|edu|us)')
+    reg = re.compile(r'\.[a-zA-Z0-9]*\.(?:com|au.uk|co\.in|co\.uk|gov|org|edu|us|it|fr|mx|net|int|mil|arpa|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|bq|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cw|cx|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|ru)')
     s1 = reg.search(url1)
     s2 = reg.search(url2)
     if not (s1 and s2): return False
-    domain1 = reg.search(url1).group(0)
-    domain2 = reg.search(url2).group(0)
-
+    domain1 = re.search(r'\.[a-zA-Z0-9]*\.', reg.search(url1).group(0)).group(0)
+    domain2 = re.search(r'\.[a-zA-Z0-9]*\.', reg.search(url2).group(0)).group(0)
+    #print(domain1, domain2) 
+    
     if print_out:
         print(1, url1)
         print(2, url2)
         print(domain1 == domain2)
     return domain1 == domain2
 
-# search a web page's text to find given phrase matches. assumes a validated url
-def search_page(url, phrases):
+
+# perform two tasks: 
+# check that a web page is in english
+# search the web page's text to find given phrase matches. assumes a validated url
+def check_page(url, phrases):
     try:
         r = requests.get(url, timeout=.1)
         page_soup = BeautifulSoup(r.text, 'html.parser')
+        content = " ".join( \
+                [x for x in [p.string for p in page_soup.find_all('p')] \
+                if x is not None])
+        if detect(content) != 'en':
+            return False
+
         for p in page_soup.find_all('p'):
             for phrase in phrases:
                 if p.string and phrase in p.string:
@@ -54,6 +64,7 @@ def search_page(url, phrases):
         return False
 
 
+
 # return a list of pages connected relevant pages branching at most to depth `max_depth`
 def branch_from_source(source_url, phrases, max_depth):
     assert(max_depth > 0)
@@ -66,9 +77,8 @@ def branch_from_source(source_url, phrases, max_depth):
         if (not valid_url(ref_tag['href'])) or same_domain(source_url, ref_tag['href'], False):
             continue
 
-        if search_page(ref_tag['href'], phrases):   
+        if check_page(ref_tag['href'], phrases):   
             print("adding "+ ref_tag['href'][0:40] + " remaining levels ", max_depth-1)
-            print(same_domain(source_url, ref_tag['href'], True))
             page_summaries.append(page_summary(ref_tag['href'], source_url))
             if max_depth > 1:
                 page_summaries.extend(branch_from_source(ref_tag['href'], phrases, max_depth - 1))
@@ -83,7 +93,7 @@ def delete_duplicate(array):
     return output
 
 def page_summary(url, parent_url):
-    return {"url": url, "parent_url": url}            
+    return {"url": url, "parent_url": parent_url}            
 
 def main(arg):
     guardian_ref = 'https://www.theguardian.com/commentisfree/2016/nov/10/after-donald-trump-win-americans-organizing-us-politics'
@@ -93,7 +103,7 @@ def main(arg):
     guardian_ref2 = "http://www.theguardian.com/us"
     facebook_ref = "https://www.facebook.com"
     
-    pages = branch_from_source(guardian_ref, ['Donald Trump', 'Trump', 'Donald', 'Election'], 3)
+    pages = branch_from_source(guardian_ref, ['Donald Trump', 'Trump', 'Donald'], 10)
     pages = delete_duplicate(pages)
     for p in pages:
         print(p)
